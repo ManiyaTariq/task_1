6db32d5ddddc42de176fa2f1b9b17ecf486087e3
ManiyaTariq <162306425+ManiyaTariq@users.noreply.github.com>
2024-05-02 18:40:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var age = 15;
+if (age < 2) {
+    console.log("You are a Baby");
+}
+else if (age >= 2 && age < 4) {
+    console.log("You are a Toddler");
+}
+else if (age >= 4 && age < 13) {
+    console.log("You are a Kid");
+}
+else if (age >= 13 && age < 20) {
+    console.log("You are a Teenager");
+}
